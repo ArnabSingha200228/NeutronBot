2ca1e144fefe785525da115888d46d1267766fe6
ArnabSingha200228 <arnabsingha200228@gmail.com>
2025-01-27 13:56:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+API_KEY = "AIzaSyBolyLRVMitWnRWCVnigpV4WNGsOeHCuCw"
+
+st.title("Neutron")
+
+model=ChatGoogleGenerativeAI(google_api_key=API_KEY,model="gemini-1.5-pro")
+st.markdown('''#### Hi! Let's get started.''')
+
+msgg=st.text_input("Type in your message for me")
+
+if len(msgg)>0:
+	prompt=ChatPromptTemplate(["Here is my message:{msg}"])
+	chain=({"msg":RunnablePassthrough()}|prompt|model|StrOutputParser())
+	reply=chain.invoke(msgg)
+	st.write(reply)
+
+footer = """
+    <style>
+        .footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: rgba(255,255,255,0.7);
+            color: #000;
+            text-align: center;
+            padding: 10px;
+            font-size: 14px;
+            box-shadow: 0 -1px 5px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+    <div class="footer">
+        👨‍💻 Developed by <a href="" target="_blank">Arnab Singha</a> | 
+        📧 <a href="mailto:arnabsingha200228@gmail.com">Contact Us</a>
+    </div>
+"""
+st.markdown(footer,True)
