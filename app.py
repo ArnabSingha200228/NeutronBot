96ae0a397225130e73b917e8e47e75679182af20
ArnabSingha200228 <arnabsingha200228@gmail.com>
2025-01-28 01:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
-api_key = os.getenv("API_KEY")
+api_key = st.secrets["API_KEY"]
 
 st.title("Neutron")
 
